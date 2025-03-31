fe35980d52e06d5d9d08acba356ecd0e9ef1fefb
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-03-31 17:58:26 +0200

    Updated Home.tsx, links and CSS to create grid for the listings

--- my-react-app/src/pages/home/home.tsx
+++ my-react-app/src/pages/home/home.tsx
@@ -1,19 +1,26 @@
-import logements from '../../data/logements.json'
-import type { Logement } from '../../types/logement'
-import './home.css'
+import logements from "../../data/logements.json";
+import "./home.scss";
+
+type Logement = {
+  id: string;
+  title: string;
+  cover: string;
+  location: string;
+  tags: string[];
+};
 
 function Home() {
-    return (
-        <div>
-            <h1>Page d'Accueil</h1>
-            Add your housing listings section
+  return (
+    <>
+      <h1>Page d'Accueil</h1>
+      Add your housing listings section
       <section className="listings">
         <h2>Available Properties</h2>
         <div className="logements-grid">
           {logements.map((logement: Logement) => (
             <article key={logement.id} className="logement-card">
-              <img 
-                src={logement.cover} 
+              <img
+                src={logement.cover}
                 alt={logement.title}
                 className="logement-cover"
               />
@@ -21,15 +28,17 @@ function Home() {
               <p>{logement.location}</p>
               <div className="tags">
                 {logement.tags.map((tag) => (
-                  <span key={tag} className="tag">{tag}</span>
+                  <span key={tag} className="tag">
+                    {tag}
+                  </span>
                 ))}
               </div>
             </article>
           ))}
         </div>
-      </section> 
-        </div>
-    )
+      </section>
+    </>
+  );
 }
 
-export default Home
+export default Home;
