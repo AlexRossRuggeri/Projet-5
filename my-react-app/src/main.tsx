f44a8d403a877c79d542b651aedba8665de2bed0
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-03-31 17:59:38 +0200

    Updated main.tsx for nested routes

--- my-react-app/src/main.tsx
+++ my-react-app/src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App.tsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Layout from "./components/common/layout/Layout";
+import Home from "./pages/home/Home";
+import Error from "./pages/error/Error";
+import About from "./pages/a-propos/About";
+import Listings from "./pages/logements/Logements";
+
+import "./main.scss";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/a-propos" element={<About />} />
+          <Route path="/fiche-logement" element={<Listings />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
