151091876c2103be52c9a45f2d4117f7773f6324
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-04-02 21:54:00 +0200

    Created Card Component

---
+++ my-react-app/src/components/logements/card/card.tsx
@@ -0,0 +1,27 @@
+import "./Card.scss";
+
+interface Logement {
+  id: string;
+  title: string;
+  cover: string;
+}
+
+interface CardProps {
+  logement: Logement;
+}
+
+function Card({ logement }: CardProps) {
+  return (
+    <article className="card">
+      <div
+        className="card__cover"
+        style={{ backgroundImage: `url(${logement.cover})` }}
+      >
+        <div className="card__overlay" />
+        <h3 className="card__title">{logement.title}</h3>
+      </div>
+    </article>
+  );
+}
+
+export default Card;
