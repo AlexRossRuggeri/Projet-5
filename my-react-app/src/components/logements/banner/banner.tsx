0b6f79925a797e6f3a90983d9b4f4188ce0e64cc
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-04-02 21:53:45 +0200

    Created Banner component

---
+++ my-react-app/src/components/logements/banner/banner.tsx
@@ -0,0 +1,24 @@
+import "./Banner.scss";
+
+interface BannerProps {
+  title?: string;
+  imageUrl: string;
+  showOverlay?: boolean;
+  overlayVariant?: "light" | "dark";
+}
+
+const Banner = ({ title, imageUrl, overlayVariant = "dark" }: BannerProps) => {
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundImage: `url('${imageUrl}')`,
+      }}
+    >
+      <div className={`banner__overlay banner__overlay--${overlayVariant}`} />
+      {title && <h1 className="banner__title">{title}</h1>}
+    </div>
+  );
+};
+
+export default Banner;
