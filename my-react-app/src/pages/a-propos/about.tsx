024a3d86fb1d32e7e719bab5f31cf5a7d7616acf
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-04-03 17:00:07 +0200

    Created About.scss

--- my-react-app/src/pages/a-propos/about.tsx
+++ my-react-app/src/pages/a-propos/about.tsx
@@ -1,11 +1,13 @@
 import Banner from "../../components/logements/banner/Banner";
 import AboutBanner from "../../assets/banner-about.webp";
 
+import "./About.scss";
+
 function About() {
   return (
-    <>
+    <main className="about">
       <Banner imageUrl={AboutBanner} showOverlay overlayVariant="light" />
-    </>
+    </main>
   );
 }
 
