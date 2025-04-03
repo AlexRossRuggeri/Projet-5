a2144120b640dc4f12becef6107428255f6141ff
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-04-03 17:01:09 +0200

    Added classNames to Footer.tsx

--- my-react-app/src/components/common/footer/Footer.tsx
+++ my-react-app/src/components/common/footer/Footer.tsx
@@ -4,9 +4,11 @@ import "./Footer.scss";
 function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer>
-      <img src={logo} alt="Logo de Kasa" className="kasa-logo" />
-      <p>&copy; {currentYear} Kasa. All rights reserved.</p>
+    <footer className="footer">
+      <img src={logo} alt="Logo de Kasa" className="footer__logo" />
+      <p className="footer__text">
+        &copy; {currentYear} Kasa. All rights reserved.
+      </p>
     </footer>
   );
 }
