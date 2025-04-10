3baf48de09de84a6394d7006afcdbb8cf3d07a70
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-04-10 19:35:11 +0200

    Update card.tsx to img instead of background image

--- my-react-app/src/components/logements/card/card.tsx
+++ my-react-app/src/components/logements/card/card.tsx
@@ -14,10 +14,14 @@ interface CardProps {
 function Card({ logement }: CardProps) {
   return (
     <Link to="/fiche-logement" className="card">
-      <div
-        className="card__cover"
-        style={{ backgroundImage: `url(${logement.cover})` }}
-      >
+      <div className="card__image-container">
+        <img
+          src={logement.cover}
+          alt={logement.title}
+          className="card__image"
+        />
+        <div className="card__overlay" />
+
         <h3 className="card__title">{logement.title}</h3>
       </div>
     </Link>
