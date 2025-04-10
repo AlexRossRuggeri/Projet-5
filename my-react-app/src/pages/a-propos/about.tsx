3f91e39e919216aa4406d75abc43052ec1e95e99
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-04-10 19:36:32 +0200

    Update about.tsx
    Remove main tag and add FAQSection

--- my-react-app/src/pages/a-propos/about.tsx
+++ my-react-app/src/pages/a-propos/about.tsx
@@ -1,13 +1,15 @@
 import Banner from "../../components/logements/banner/Banner";
 import AboutBanner from "../../assets/banner-about.webp";
+import FAQSection from "../../components/FAQ/FAQSection";
 
 import "./About.scss";
 
 function About() {
   return (
-    <main className="about">
+    <>
       <Banner imageUrl={AboutBanner} showOverlay overlayVariant="light" />
-    </main>
+      <FAQSection />
+    </>
   );
 }
 
