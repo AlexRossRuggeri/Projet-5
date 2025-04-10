5674c7a7091e9f09ba02180afac56e64e7f2f302
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-04-10 19:17:58 +0200

    Add link to the Header Logo

--- my-react-app/src/components/common/header/Header.tsx
+++ my-react-app/src/components/common/header/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import logo from "../../../assets/LOGO.svg";
 import NavLinks from "../navigation/NavLinks";
 import "./Header.scss";
@@ -5,7 +6,9 @@ import "./Header.scss";
 function Header() {
   return (
     <header>
-      <img src={logo} alt="Logo de Kasa" className="kasa-logo" />
+      <Link to="/">
+        <img src={logo} alt="Logo de Kasa" className="kasa-logo" />
+      </Link>
       <NavLinks />
     </header>
   );
