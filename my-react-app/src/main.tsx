bb152828e3ef7a09f69b8ed768b5316c8f5af2d9
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-04-14 16:19:04 +0200

    Mise à jour des imports suite au renommage des fichiers

--- my-react-app/src/main.tsx
+++ my-react-app/src/main.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Layout from "./components/common/layout/Layout";
-import Home from "./pages/home/Home";
-import Error from "./pages/error/Error";
-import About from "./pages/a-propos/About";
-import Listings from "./pages/logements/Logements";
+import Layout from "./components/common/Layout/Layout";
+import Home from "./pages/Home/Home";
+import Error from "./pages/Error/Error";
+import About from "./pages/About/About";
+import FicheLogement from "./pages/HousingFile/HousingFile";
 
 import "./Main.scss";
 
@@ -18,7 +18,7 @@ root.render(
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/a-propos" element={<About />} />
-          <Route path="/fiche-logement" element={<Listings />} />
+          <Route path="/fiche-logement/:id" element={<FicheLogement />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
