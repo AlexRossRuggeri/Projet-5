8f1f0802b07893b0644af7094c561b215bc97fdd
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-04-14 16:20:12 +0200

    Creation de la page Error et le SCSS associé pour correspondre au Figma
    - Title
    - Text
    - Lien page d'accueil

--- my-react-app/src/pages/error/error.tsx
+++ my-react-app/src/pages/error/error.tsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
+import "./Error.scss";
+
 function Error() {
-    return (
-        <div>
-            <h1>Error 404</h1>
-        </div>
-    )
+  return (
+    <div className="error-section">
+      <h2 className="error-section__title">404</h2>
+      <p className="error-section__text">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link className="error-section__link" to="/">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
 }
 
-export default Error
+export default Error;
