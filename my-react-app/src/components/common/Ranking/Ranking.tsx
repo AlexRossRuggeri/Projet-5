5f87ec34322b56e8e5ddab35e441610ad70e95c3
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-04-15 13:46:58 +0200

    Created the Ranking Component

---
+++ my-react-app/src/components/common/Ranking/Ranking.tsx
@@ -0,0 +1,29 @@
+import "./Ranking.scss";
+
+interface RatingProps {
+  rating: string;
+}
+
+function RatingReview({ rating }: RatingProps) {
+  const numericRating = parseInt(rating);
+  return (
+    <div>
+      {[1, 2, 3, 4, 5].map((star) => {
+        return (
+          <span
+            className="star"
+            style={{
+              color: numericRating >= star ? "#FF6060" : "#E3E3E3",
+              fontSize: `35px`,
+            }}
+          >
+            {" "}
+            <i className="fa-solid fa-star" />{" "}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
+
+export default RatingReview;
